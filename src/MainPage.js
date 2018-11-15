a071300868beb3b6b6fd5f547627a7234af379ad
allenfortner <allenfortner99@gmail.com>
2018-11-15 17:21:57 -0500

    Add routes for main page and search page

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Bookshelf from './Bookshelf';
+import { Link } from 'react-router-dom';
 
 class MainPage extends Component {
 	state = {}
@@ -18,7 +19,7 @@ class MainPage extends Component {
 						</div>
 					</div>
 				<div className="open-search">
-					<a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+					<Link to="/search">Add a book</Link>
 				</div>
 			</div>
 		)
