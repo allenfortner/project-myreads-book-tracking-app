009dd3106a9c7a9aa549e0cc8f0a9ed0415001a4
allenfortner <allenfortner99@gmail.com>
2018-11-15 17:50:02 -0500

    Get book data from BooksAPI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI';
 import './App.css';
 import MainPage from './MainPage';
 import SearchPage from './SearchPage';
 import { Route } from 'react-router-dom';
 
 class BooksApp extends Component {
-  state = {}
-
-  render() {
-    return (
-	<div className="app">
-		<Route exact path="/" render={() => (
-			<MainPage />
-		)}/>
-		<Route exact path="/search" render={() => (
-			<SearchPage />
-		)}/>
-	</div>
-    )
-  }
+	state = {
+		books: []
+	}
+  
+	componentDidMount = () => {
+		//As soon as the component is mounted, get the list of books using getBooks
+		this.getBooks();
+	}
+  
+	getBooks = () => {
+		//Get all of the books from BooksAPI and set the books state to the list we just received
+		BooksAPI.getAll().then((newBooks) => {
+			console.log(newBooks);
+			this.setState({books: newBooks});
+		});
+	}
+	
+	render() {
+		return (
+			<div className="app">
+				<Route exact path="/" render={() => (
+					<MainPage />
+				)}/>
+				<Route exact path="/search" render={() => (
+					<SearchPage />
+				)}/>
+			</div>
+		)
+	}
 }
 
 export default BooksApp;
